77a740dab22b61472caa73f3af121437c68f4341
Anne <95251413+AnneH20@users.noreply.github.com>
2025-12-29 11:51:48 -0600

    Delpoy Flutter Web

--- android/app/src/main/kotlin/com/pert/pert/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pert.pert
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/models/estimate_type.dart
+++
@@ -1,5 +0,0 @@
-enum EstimateType {
-  optimistic,
-  mostLikely,
-  pessimistic,
-}
